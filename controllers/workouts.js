19c40ae35f7285c7d2cfdbc0024b2b18ee37b11f
harryhmohney <harrison.mohney@gmail.com>
2023-08-10 12:12:13 -0700

    index redirect successful

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -5,6 +5,25 @@ module.exports = {
 
 const Exercise = require('../models/workout')
 
+
+
+function newWorkout(req, res) {
+    res.render('workouts/new', { title: 'Add A Workout' })
+}
+
+async function create(req, res) {
+    try {
+        await Exercise.create(req.body)
+        res.redirect('/')
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+
+
+
 // function newWorkout(req, res) {
 //     res.render('workouts/new', {title: 'Add A Workout'})
 // }
@@ -17,16 +36,3 @@ const Exercise = require('../models/workout')
 //       // res.redirect('/workouts/new')
 //     } catch(err){console.log(err)}
 // }
-
-function newWorkout(req, res) {
-    res.render('workouts/new', { title: 'Add A Workout' })
-}
-
-async function create(req, res) {
-    try {
-        await Exercise.create(req.body)
-        res.redirect('/workouts/new')
-    } catch (err) {
-        console.log(err)
-    }
-}
